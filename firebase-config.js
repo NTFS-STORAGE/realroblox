38c8b7f28870269c6b330281a892c91d28df0378
NTFS'S Storage <103947463+NTFS-STORAGE@users.noreply.github.com>
2024-12-25 20:44:10 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,9 @@
-// firebase-config.js
+// Import Firebase modules (Modular SDK)
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
+import { getFirestore, setDoc, doc, getDoc, query, where, getDocs } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
+
+// Firebase config object
 const firebaseConfig = {
   apiKey: "AIzaSyBpiU-R3cvsW_lvnUPFpHaluPQFhEQLAUo",
   authDomain: "registerteamextreme.firebaseapp.com",
@@ -9,6 +14,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and Firestore
+const auth = getAuth(app);
+const db = getFirestore(app);
